e1c446fdfb664cf3cac5189da37f1380336bd4d5
rtperezyera <tllezroberto9@gmail.com>
2020-06-02 14:16:41 -0500

    Init project
    Upload project directory

---
+++ myhealthjournal/myhealthjournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myhealthjournal
+//
+//  Created by rtperezyera on 02/06/20.
+//  Copyright © 2020 LifeHealthSolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
